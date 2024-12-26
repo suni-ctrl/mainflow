3998b10b53500ccad500595c1af85bc985b82257
suni-ctrl <sunithakutty1432@gmail.com>
2024-12-26 18:41:53 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,39 @@
+
+my_list = [1, 2, 3, 4]
+print("Original List:", my_list)
+my_list.append(5)
+print("List after adding an element:", my_list)
+my_list.remove(2)
+print("List after removing an element:", my_list)
+
+# Modifying an element in the list
+my_list[0] = 10
+print("List after modifying an element:", my_list)
+
+# Creating a dictionary
+my_dict = {"name": "Kutty", "age": 22}
+print("\nOriginal Dictionary:", my_dict)
+
+# Adding a key-value pair
+my_dict["city"] = "Chittoor"
+print("Dictionary after adding a key-value pair:", my_dict)
+
+# Removing a key-value pair
+del my_dict["age"]
+print("Dictionary after removing a key-value pair:", my_dict)
+
+# Modifying a value
+my_dict["name"] = "Uday Kumar"
+print("Dictionary after modifying a value:", my_dict)
+
+# Creating a set
+my_set = {1, 2, 3}
+print("\nOriginal Set:", my_set)
+
+# Adding an element to the set
+my_set.add(4)
+print("Set after adding an element:", my_set)
+
+# Removing an element from the set
+my_set.remove(2)
+print("Set after removing an element:", my_set)
